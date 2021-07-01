2a36098b636ada06686cae177488b338b434bdf9
YaroslavPushkarev <yarik2015@protonmail.com>
2021-07-01 09:17:39 +0300

    1

---
+++ go-getting-started/go.mod
@@ -0,0 +1,8 @@
+module github.com/heroku/go-getting-started
+
+go 1.12
+
+require (
+	github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f
+	github.com/heroku/x v0.0.0-20171004170240-705849e307dd
+)
---
+++ go-getting-started/main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"sort"
+	"strings"
+	"text/template"
+	"time"
+
+	"github.com/bmizerany/pat"
+
+	// "github.com/gin-gonic/gin"
+
+	_ "github.com/heroku/x/hmetrics/onload"
+)
+
+type Joke struct {
+	ID    string `json:"id"`
+	Title string `json:"title"`
+	Score int    `json:"score"`
+	Body  string `json:"body"`
+}
+
+var jokes []Joke
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*.html"))
+}
+func index(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "index.html", nil)
+}
+
+func getJokes(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "index.html", jokes)
+}
+
+func getJoke(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get(":id")
+	for _, item := range jokes {
+		if item.ID == id {
+			tpl.ExecuteTemplate(w, "id.html", item)
+			return
+		}
+	}
+}
+
+func randomJokes(w http.ResponseWriter, r *http.Request) {
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	rand.Shuffle(len(jokes), func(i, j int) { jokes[i], jokes[j] = jokes[j], jokes[i] })
+
+	tpl.ExecuteTemplate(w, "random.html", jokes)
+}
+
+func fun(w http.ResponseWriter, r *http.Request) {
+	sort.SliceStable(jokes, func(i, j int) bool {
+		return jokes[i].Score > jokes[j].Score
+	})
+	tpl.ExecuteTemplate(w, "fun.html", jokes)
+}
+
+func search(w http.ResponseWriter, r *http.Request) {
+	value := r.FormValue("q")
+
+	for _, item := range jokes {
+		contain := strings.Contains(item.Title, value)
+		if contain == true {
+			tpl.ExecuteTemplate(w, "search.html", item)
+			return
+		}
+	}
+}
+
+func main() {
+
+	content, _ := ioutil.ReadFile("reddit_jokes.json")
+	json.Unmarshal(content, &jokes)
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal(":8000")
+	}
+
+	r := pat.New()
+
+	r.Get("/", http.HandlerFunc(index))
+	r.Get("/jokes", http.HandlerFunc(getJokes))
+	r.Get("/jokes/:id", http.HandlerFunc(getJoke))
+	r.Get("/jokes/random/", http.HandlerFunc(randomJokes))
+	r.Get("/jokes/funniest/", http.HandlerFunc(fun))
+	r.Get("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
+	r.Get("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	r.Post("/search", http.HandlerFunc(search))
+	log.Fatal(http.ListenAndServe(":"+port, r))
+}
