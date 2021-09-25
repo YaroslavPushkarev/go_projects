77612389a6b5dd3433ab2b26261f306138801bb4
YaroslavPushkarev <yarik2015@protonmail.com>
2021-09-25 11:20:23 +0300

    feat: add tests cases

--- go-getting-started/controllers/insert_test.go
+++ go-getting-started/controllers/insert_test.go
@@ -11,19 +11,38 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func TestInsertData(t *testing.T) {
-	clientOptions := options.Client().ApplyURI("mongodb+srv://jokesdb:jokesdb@joke.kxki9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	client, err := mongo.Connect(ctx, clientOptions)
-
-	if err != nil {
-		log.Fatal(err)
+func TestControllers_InsertData(t *testing.T) {
+	tt := []struct {
+		name  string
+		title string
+	}{
+		{
+			name:  "title string",
+			title: "3",
+		},
+		{
+			name:  "empty title",
+			title: "",
+		},
 	}
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
 
-	collection := client.Database("Jokes").Collection("jokes")
-	res, err := InsertData(collection, Joke{"asdada", "Sdfs", 3, "4324"})
-	assert.Nil(t, err)
-	assert.IsType(t, &mongo.InsertOneResult{}, res)
+			clientOptions := options.Client().ApplyURI("mongodb+srv://jokesdb:jokesdb@joke.kxki9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+			ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+			defer cancel()
+
+			client, err := mongo.Connect(ctx, clientOptions)
+
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			collection := client.Database("Jokes").Collection("jokes")
+			res, err := InsertData(collection, Joke{"asdada", tc.title, 3, "4324"})
+
+			assert.Nil(t, err)
+			assert.IsType(t, &mongo.InsertOneResult{}, res)
+		})
+	}
 }
